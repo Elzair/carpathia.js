435a0fa297604ea169be19a58bf02ab4e1c39636
Philip Woods <elzairthesorcerer@gmail.com>
2014-09-23 09:55:22 -0400

    Converted unit tests to use chai.expect

--- test/test.js
+++ test/test.js
@@ -1,11 +1,14 @@
-var assert = require("assert")
-  , carpathia = require(__dirname + "/../")
+var carpathia = require(__dirname + "/../")
+  , chai      = require("chai")
+  , expect    = chai.expect
   ;
+
+chai.should();
 
 describe("Carpathia.js", function() {
   describe("translate", function() {
     it("should correctly translate the following phrases", function() {
-      var test_translations = {
+      var test = {
           "en-US": {
             "phrase": "that's life"
           }
@@ -13,83 +16,80 @@ describe("Carpathia.js", function() {
             "phrase": "c'est la vie"
           }
       };
-      assert.strictEqual(carpathia.translate(test_translations, "en-US", "phrase"), "that's life");
-      assert.strictEqual(carpathia.translate(test_translations, "fr-FR", "phrase"), "c'est la vie");
+      expect(test).to.be.an("object");
+      expect(test).to.have.property("en-US");
+      expect(test["en-US"]).to.be.an('object');
+      expect(test["en-US"]).to.have.property("phrase");
+      expect(test["en-US"].phrase).to.be.a("string");
+      expect(test["en-US"].phrase).to.equal("that's life");
     });
 
-    it('should throw an invalid dictionary error on missing dictionary', function(done) {
+    it("should throw an invalid dictionary error on missing dictionary", function(done) {
       try {
         carpathia.translate(null, "en-US", "phrase");
       }
       catch (err) {
-        assert(err instanceof Error);
-        assert(/Invalid dictionary/.test(err));
+        expect(err).to.match(/Invalid dictionary/);
         done();
       }
     });
 
-    it('should throw an invalid dictionary error on non-object dictionary', function(done) {
+    it("should throw an invalid dictionary error on non-object dictionary", function(done) {
       try {
         carpathia.translate(5, "en-US", "phrase");
       }
       catch (err) {
-        assert(err instanceof Error);
-        assert(/Invalid dictionary/.test(err));
+        expect(err).to.match(/Invalid dictionary/);
         done();
       }
     });
 
-    it('should throw an invalid language error on empty language parameter', function(done) {
+    it("should throw an invalid language error on empty language parameter", function(done) {
       try {
         carpathia.translate({"en-US": {"phrase": "foo"}}, null, "phrase");
       }
       catch (err) {
-        assert(err instanceof Error);
-        assert(/Invalid language/.test(err));
+        expect(err).to.match(/Invalid language/);
         done();
       }
     });
 
-    it('should throw an invalid language error on missing language parameter', function(done) {
+    it("should throw an invalid language error on missing language parameter", function(done) {
       try {
         carpathia.translate({"en-US": {"phrase": "foo"}}, "baz", "phrase");
       }
       catch (err) {
-        assert(err instanceof Error);
-        assert(/Invalid language/.test(err));
+        expect(err).to.match(/Invalid language/);
         done();
       }
     });
 
-    it('should throw an invalid language error on invalid language parameter', function(done) {
+    it("should throw an invalid language error on invalid language parameter", function(done) {
       try {
         carpathia.translate({"en-US": {"phrase": "foo"}}, "fr-FR", "phrase");
       }
       catch (err) {
-        assert(err instanceof Error);
-        assert(/Invalid language/.test(err));
+        expect(err).to.match(/Invalid language/);
         done();
       }
     });
 
-    it('should throw an invalid symbol error on missing symbol parameter', function(done) {
+    it("should throw an invalid symbol error on missing symbol parameter", function(done) {
       try {
         carpathia.translate({"en-US": {"phrase": "foo"}}, "en-US", null);
       }
       catch (err) {
-        assert(err instanceof Error);
-        assert(/Invalid symbol/.test(err));
+        expect(err).to.match(/Invalid symbol/);
         done();
       }
     });
 
-    it('should throw an invalid symbol error on invalid symbol parameter', function(done) {
+    it("should throw an invalid symbol error on invalid symbol parameter", function(done) {
       try {
         carpathia.translate({"en-US": {"phrase": "foo"}}, "en-US", "clause");
       }
       catch (err) {
-        assert(err instanceof Error);
-        assert(/Invalid symbol/.test(err));
+        expect(err).to.match(/Invalid symbol/);
         done();
       }
     });
